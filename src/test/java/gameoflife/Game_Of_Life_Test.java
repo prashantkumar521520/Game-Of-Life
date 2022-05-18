21c31fb1113ca2283bea5b62fc21d8eb4e021286
Prashant Kumar <prashantkumar@Prashants-MacBook-Pro.local>
2022-05-18 12:58:06 +0530

    [Prashant] Testing code for boat patttern Passing test

--- src/test/java/gameoflife/Game_Of_Life_Test.java
+++ src/test/java/gameoflife/Game_Of_Life_Test.java
@@ -15,4 +15,13 @@ public class Game_Of_Life_Test {
 
         assertArrayEquals(new int[][]{{1, 1}, {1, 2}, {2, 1}, {2, 2}}, nextGeneration.toArray());
     }
+
+    @Test
+    void boatPattern() {
+        Game_Of_Life game_of_life = new Game_Of_Life(new int[][]{{0, 1}, {1, 0}, {2, 1}, {0, 2}, {1, 2}});
+
+        List<int[]> nextGeneration = game_of_life.getNextGeneration();
+
+        assertArrayEquals(new int[][]{{0, 1}, {0, 2}, {1, 0}, {1, 2}, {2, 1}}, nextGeneration.toArray());
+    }
 }
